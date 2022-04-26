dd3d72b314883e4296bee17fc4b4fc44165b9482
LeeZy <leevy1402@gmail.com>
2022-04-26 08:13:19 +0700

    Phuong trinh & Animal

---
+++ Animal/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Cat_170 cat_170 = new Cat_170();
+            cat_170.eat_170();
+            cat_170.makeSound_170();
+            cat_170.run();
+            Bird_170 bird_170 = new Bird_170();
+            bird_170.eat_170();
+            bird_170.makeSound_170();
+            bird_170.fly_170();
+            Console.ReadKey();
+        }
+    }
+    class Animal
+    {
+        public virtual void eat_170()
+        {
+            Console.WriteLine("Đồ ăn");
+        }
+        public virtual void makeSound_170()
+        {
+            Console.WriteLine("");
+        }
+    }
+    class Cat_170 : Animal
+    {
+        public void run()
+        {
+            Console.WriteLine("Mèo chạy");
+
+        }
+        public override void eat_170()
+        {
+            Console.WriteLine("Mèo ăn cá");
+        }
+        public override void makeSound_170()
+        {
+            Console.WriteLine("Mèo kêu meo meo");
+        }
+    }
+    class Bird_170 : Animal
+    {
+        public void fly_170()
+        {
+            Console.WriteLine("Chim bay cao");
+
+        }
+        public override void eat_170()
+        {
+            Console.WriteLine("Chim ăn sâu");
+        }
+        public override void makeSound_170()
+        {
+            Console.WriteLine("Chít chít");
+        }
+    }
+}
---
+++ BT_Diem_Danh_26_4/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BT_Diem_Danh_26_4
+{ 
+    class PhuongTrinh
+    {
+        private double _a_170 { set; get; }
+        private double _b_170 { set; get; }
+        private double _c_170 { set; get; }
+
+        public PhuongTrinh()
+        {
+            _a_170 = 1;
+            _b_170 = 2;
+            _c_170 = 3;
+        }
+        public PhuongTrinh(double a, double b, double c)
+        {
+            _a_170 = a;
+            _b_170 = b;
+            _c_170 = c;
+        }
+        public double Delta(double a_170, double b_170, double c_170)
+        {
+            double delta_170;
+            delta_170 = b_170 * b_170 - 4 * a_170 * c_170;
+            return delta_170;
+        }
+        public void nghiem(double a_170, double b_170, double c_170, double delta_170)
+        {
+            if (delta_170 > 0)
+            {
+                Console.WriteLine("Phuong trinh co hai nghiem : ");
+                Console.WriteLine("X1 = {0}", ((-b_170 - Math.Sqrt(delta_170)) / 2 * a_170));
+                Console.WriteLine("X2 = {0}", ((-b_170 + Math.Sqrt(delta_170)) / 2 * a_170));
+
+            }
+            else if (delta_170 == 0)
+            {
+                Console.WriteLine("Phuong trinh co hai nghiem kep nghiem");
+                Console.WriteLine("X1 = X2 {0}", -b_170 / 2 * a_170);
+            }
+            else if (delta_170 < 0)
+            {
+                Console.WriteLine("Phuong trinh vo nghiem");
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            double a_170, b_170, c_170, delta_170;
+            Console.WriteLine("Nhập a, b, c: ");
+            string[] so_170 = Console.ReadLine().Split(' ');
+            a_170 = float.Parse(so_170[0]);
+            b_170 = float.Parse(so_170[1]);
+            c_170 = float.Parse(so_170[2]);
+            PhuongTrinh pt = new PhuongTrinh();
+            delta_170 = pt.Delta(a_170, b_170, c_170);
+            pt.nghiem(a_170, b_170, c_170, delta_170);
+            Console.ReadKey();
+        }
+    }
+}
